f7088b70179e172c091206ca801e553dc0b7a4ef
itIsInspiring <guillaumedeschamps75@gmail.com>
2015-12-17 13:41:59 +0100

    finalisation page compétence

--- template-competences.php
+++ template-competences.php
@@ -7,19 +7,18 @@
 
 <?php while (have_posts()) : the_post(); ?>
   <article <?php post_class('row'); ?>>
-    
+    <div class="medium-12 columns">
     <header>
       <h1 class="entry-title uppercase"><?php the_title(); ?><span class="trait-lozanges"></span></h1>
     </header>
     
     <div class="entry-content row">
-         <aside id="competence-infos" class=" medium-4 columns">
+         <aside id="competence-infos" class="small-10 small-centered medium-4 medium-uncentered columns">
              <h3>Projets concernés : </h3>
              <?php
                 $competence = $post->post_name ;
                 $args= array(
                     'post_type' => 'projet',
-                    'order' => 'ASC',
                     'posts_per_page' => -1,
                     'tax_query' => array(
                         array(
@@ -28,7 +27,6 @@
                             'terms' => $competence,
                         )
                     )
-				    
                 );
              
                 // The Query
@@ -54,68 +52,80 @@
                 wp_reset_postdata();
              ?>
          </aside>    
-          <div id="Comptence-main-content" class=" medium-8 columns">
+          <div id="Comptence-main-content" class="small-10 small-centered medium-8 medium-uncentered columns">
               <?php the_content(); ?>
           </div>
     </div>
 
         
     <section class="related-projects row" >
-        <h3 class="text-center"><?php 
-            /* récupérer le titre de la page*/  
-            if(is_page( 'Comédien' )){
-                echo "Alberto a joué dans...";
-            } elseif(is_page( 'Auteur' )){
-                echo "Alberto a écrit dans...";
-            } elseif(is_page( 'Animateur' )){
-                echo "Alberto est intervenu dans...";
-            }
-        ?>    
-        </h3>
-        <ul>
-           <?php
-               $competence = $post->post_name ;
-                $args= array(
-                    'post_type' => 'projet',
-                    'order' => 'ASC',
-                    'posts_per_page' => -1,
-                    'tax_query' => array(
-                        array(
-                            'taxonomy' => 'Compétences',
-                            'field' => 'slug',
-                            'terms' => $competence,
+       <div>
+           
+            <h3 class="text-center"><?php 
+                /* récupérer le titre de la page*/  
+                if(is_page( 'Comédien' )){
+                    echo "Alberto a joué dans...";
+                } elseif(is_page( 'Auteur' )){
+                    echo "Alberto a écrit dans...";
+                } elseif(is_page( 'Animateur' )){
+                    echo "Alberto est intervenu dans...";
+                }
+            ?>    
+            </h3>
+            
+            <ul class="row">
+               <?php
+                    $competence = $post->post_name ;
+                    $args_related= array(
+                        'post_type' => 'projet',
+                        'order' => 'ASC',
+                        'posts_per_page' => 3,
+                        'tax_query' => array(
+                            array(
+                                'taxonomy' => 'Compétences',
+                                'field' => 'slug',
+                                'terms' => $competence,
+                            )
                         )
-                    )
-				    
-                );
-             
-                // The Query
-                $the_query = new WP_Query( $args );
-            ?>
-            <li>
-               <a href="">
-                    <img src="<?php echo get_template_directory_uri(); ?>/dist/images/vignette.jpg" alt="description de la vignette"/>
-                    <h4>Titre de la pièce</h4>
-                    <span>juin 2014</span>
-                </a>
-            </li>
-            <li>
-               <a href="">
-                    <img src="<?php echo get_template_directory_uri(); ?>/dist/images/vignette.jpg" alt="description de la vignette"/>
-                    <h4>Titre de la pièce</h4>
-                    <span>juin 2014</span>
-                </a>
-            </li>
-            <li>
-               <a href="">
-                    <img src="<?php echo get_template_directory_uri(); ?>/dist/images/vignette.jpg" alt="description de la vignette"/>
-                    <h4>Titre de la pièce</h4>
-                    <span>juin 2014</span>
-                </a>
-            </li>
-        </ul>
+                    );
+
+                    // The Query
+                    $the_query_related = new WP_Query( $args_related );
+                    if ( $the_query_related->have_posts() ) {
+                         while ( $the_query_related->have_posts() ) {
+                            $the_query_related->the_post();
+                ?>
+
+                <li class="small-centered small-10  medium-4 medium-uncentered columns">
+                  <div class="wrapper-related">
+                   <a href="<?php the_permalink() ?>"  >
+                        <h4><?php the_title(); ?></h4>
+                        <span>
+                        <?php
+                        $infosprojet = get_post_meta($post->ID, 'projet_duree', true);
+                        foreach($infosprojet as $infoprojet){
+                            echo $infoprojet['annee-de-creation'];
+                        }
+                             ?>
+                        </span>
+                    </a>
+                    </div>
+                </li>
+
+                <?php
+                         }
+                              } else {
+                        // no posts found
+                        echo "<li>Il n'y a pas de projet dans cette catégorie pour le moment.</li>";
+                    }
+                    /* Restore original Post Data */
+                    wp_reset_postdata();
+                 ?>
+            </ul>
+        
+        </div>
     </section>
-    
+    </div>
  </article>
                                            
 <div class="last-section-deco"></div>
