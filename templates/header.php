52fcde0987615276c60f317ee9b878b544e04b82
itIsInspiring <guillaumedeschamps75@gmail.com>
2015-12-22 17:15:45 +0100

    menu responsiv ok

--- templates/header.php
+++ templates/header.php
@@ -1,3 +1,39 @@
+<div id="small-menu" class="fixed show-for-small-only ">
+     <nav class="top-bar " data-topbar role="navigation">
+      <ul class="title-area">
+        <li class="name">
+          <h1 ><a href="#">Alberto Lombardo</a></h1>
+        </li>
+         <!-- Remove the class "menu-icon" to get rid of menu icon. Take out "Menu" to just have icon alone -->
+        <li class="toggle-topbar menu-icon"><a href="#"><span></span></a></li>
+      </ul>
+    <?php
+
+    $defaults = array(
+        'theme_location'  => '',
+        'menu'            => '',
+        'container'       => 'nav',
+        'container_class' => 'top-bar-section',
+        'container_id'    => '',
+        'menu_class'      => 'menu ',
+        'menu_id'         => '',
+        'echo'            => true,
+        'fallback_cb'     => 'wp_page_menu',
+        'before'          => '',
+        'after'           => '',
+        'link_before'     => '',
+        'link_after'      => '',
+        'items_wrap'      => '<ul id="%1$s" class="%2$s">%3$s</ul>',
+        'depth'           => 0,
+        'walker'          => ''
+    );
+
+    wp_nav_menu( $defaults );
+
+    ?>
+    </nav>
+</div>
+   
 <?php
     if(is_home()){
 ?>
@@ -14,20 +50,20 @@
     }
 ?>
 
-<nav class="main-menu1 show-for-medium-up">
+<nav class="main-menu1 show-for-medium-up ">
     <div data-magellan-expedition="fixed">
         <dl class="sub-nav">
-            <dd data-magellan-arrival="about"><a href="#about">À propos</a></dd>
+            <dd data-magellan-arrival="about"><a href="/#about">À propos</a></dd>
+            <dd class="menu-lozange "><span>&loz;</span></dd>
+            <dd data-magellan-arrival="gallery"><a href="/#gallery">Galerie</a></dd>
             <dd class="menu-lozange"><span>&loz;</span></dd>
-            <dd data-magellan-arrival="gallery"><a href="#gallery">Galerie</a></dd>
+            <dd data-magellan-arrival="top" class=""><a href="/#intro" class="title-area">Alberto Lombardo</a></dd>
             <dd class="menu-lozange"><span>&loz;</span></dd>
-            <dd data-magellan-arrival="top"><a href="#intro">Alberto Lombardo</a></dd>
+            <dd data-magellan-arrival="news"><a href="/#news">À venir</a></dd>
             <dd class="menu-lozange"><span>&loz;</span></dd>
-            <dd data-magellan-arrival="news"><a href="#news">À venir</a></dd>
-            <dd class="menu-lozange"><span>&loz;</span></dd>
-            <dd data-magellan-arrival="contact"><a href="#contact">Contact</a></dd>
+            <dd data-magellan-arrival="contact"><a href="/#contact">Contact</a></dd>
         </dl>
     </div>
 </nav>
 
- 
+
